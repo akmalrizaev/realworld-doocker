3892ae81300ac90ab76a59a2839e3843994fcd1f
Akmal Rizayev <akmaljon.rizayev@gmail.com>
2022-02-05 10:30:34 +0500

    add database

--- api/src/index.js
+++ api/src/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
+const { port, host } = require("./configuration");
+const { connectDb } = require("./helpers/db");
 
 const app = express();
-
-const port = process.env.PORT
-const host = process.env.HOST
 
 app.get("/test", (req, res) => {
   res.send("Our api server is working correctly");
 });
 
-app.listen(port, () => {
-  console.log(`Started api services on port ${port}`);
-  console.log(`On host ${host}`)
-}); 
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Started api service on port ${port}`);
+    console.log(`Our host is ${host}`);
+  });
+};
 
-
+connectDb()
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer);
